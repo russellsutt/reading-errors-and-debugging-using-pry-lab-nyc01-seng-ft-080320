82a33cbac0ae2431b38b192c9dc8e7a341e07fec
Russell Sutter <russell.sutter@gmail.com>
2020-07-03 22:50:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,10 +11,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num == 1
+  if num = 1
     binding.pry
     "YUM YUM MUNCH MUNCH MUNCH"
-  else num == 2
+  else num = 2
     binding.pry
     "HAM HAM HAM IN MY TUMMY"
   end
