74443e0d09ce6461116ca7af473896aa17e9fd41
Russell Sutter <russell.sutter@gmail.com>
2020-07-03 23:35:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,9 +3,9 @@ require 'pry'
 def snake_it_up(string)
   if string[0] == s
     binding.pry
-   print 10 * s + string
+      print 10 * s + string
   else
     binding.pry
-    string
+      string
   end
 end
