63cc31c2a898f3c6faf19e33853c7a515d55c18f
Russell Sutter <russell.sutter@gmail.com>
2020-07-03 23:06:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,8 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def start_game(player1 = "Player 1", player2 = "Player 2")
   "Hello #{player1} & #{playar2}"
+  binding.pry
 end
 
 def play_game(player2)
