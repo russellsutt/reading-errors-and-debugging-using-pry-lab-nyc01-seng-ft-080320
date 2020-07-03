7db06b8ced33607de8c907a9844b6a798298ef67
Russell Sutter <russell.sutter@gmail.com>
2020-07-03 23:07:25 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 def start_game(player1 = "Player 1", player2 = "Player 2")
   "Hello #{player1} & #{playar2}"
-  binding.pry
 end
 
-def play_game(player2)
+def play_game(player1 = "Player 1", player2 = "Player 2")
   "#{player1} is better than #{player2}"
 end
