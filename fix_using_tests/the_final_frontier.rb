a31aa85fd26be0aae1fdc51d6bab5d73d2496a6b
Russell Sutter <russell.sutter@gmail.com>
2020-07-03 23:52:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,8 +3,8 @@ def generate_star_date
 end
 
 
-def state_log(generate_star_date)
-  "Captain's Log, star date #{generate_star_date}."
+def state_log(star_date)
+  "Captain's Log, star date #{star_date}."
 end
 
 def engage
