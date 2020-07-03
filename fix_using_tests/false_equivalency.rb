c96a7873cbd16ed442c739e34a9e18d61c9b231d
Russell Sutter <russell.sutter@gmail.com>
2020-07-03 22:52:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,16 +1,16 @@
 require 'pry'
 
-def get_user_input
-  gets.chomp
-end
-
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
 
-def selection(num)
+def get_user_input
+  gets.chomp
+end
+
+def selection(get_user_input)
   if num = 1
     binding.pry
     "YUM YUM MUNCH MUNCH MUNCH"
